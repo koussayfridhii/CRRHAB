312c4f24ad8d795bccfaa164c4f70dec9f945d28
koussayfridhii <koussayfridhi@issatkas.u-kairouan.tn>
2024-06-05 15:30:20 +0100

    dynamic gallerydata,

--- front/src/components/slider/Slider.jsx
+++ front/src/components/slider/Slider.jsx
@@ -1,101 +1,42 @@
 import React, { useState, useRef } from "react";
 import "./Slider.scss";
+import { useCallApi } from "../../hooks/useCallApi";
+import Spinner from "../spinner/Spinner";
 
 const Slider = ({ language }) => {
-  const [slides, setSlides] = useState([
-    {
-      title: { fr: "coccinelle", en: "", ar: "" },
-      color: "#B94014",
-      active: 0,
-      img: "IMG_20240503_095557.jpg",
-    },
-    {
-      title: { fr: "Chrysope Verte", en: "", ar: "" },
-      color: "#10170D",
-      active: 0,
-      img: "IMG_20240503_095942.jpg",
-    },
-    {
-      title: { fr: "Dasysyrphus", en: "", ar: "" },
-      color: "#EFF18F",
-      active: 0,
-      img: "IMG_20240518_094752.jpg",
-    },
-    {
-      title: { fr: "Abricot", en: "", ar: "" },
-      color: "#F4935F",
-      active: 0,
-      img: "L11A12 3 076.jpg",
-    },
-    {
-      title: { fr: "Tomate", en: "", ar: "" },
-      color: "#792521",
-      active: 0,
-      img: "371148550_194060910353349_2673705428630603414_n.jpg",
-    },
-    {
-      title: { fr: "Cerise", en: "", ar: "" },
-      color: "#E00711",
-      active: 0,
-      img: "IMG_20180527_110614.jpg",
-    },
-    {
-      title: { fr: "Aubergine", en: "", ar: "" },
-      color: "#3E670B",
-      active: 0,
-      img: "IMG_20180215_120330_HDR.jpg",
-    },
-  ]);
   const containerRef = useRef(null);
   const [active, setActive] = useState(false);
   const [index, setIndex] = useState(0);
+
+  const { data, error, isLoading } = useCallApi("media");
+
+  if (isLoading) {
+    return <Spinner />;
+  }
+
+  if (error) {
+    return <div>Error fetching data: {error.message}</div>;
+  }
+
   const swipe = (dir) => {
+    setActive((prev) => !prev);
+
     if (dir === "prev") {
-      if (index > 0) {
-        setIndex((prev) => prev - 1);
-        setActive((prev) => !prev);
-        setTimeout(() => {
-          containerRef.current.style.left = `-${index * 101}%`;
-          containerRef.current.style.backgroundColor = slides[index]?.color;
-        }, 500);
-        setTimeout(() => {
-          setActive((prev) => !prev);
-        }, 1000);
-      } else {
-        setIndex(slides.length - 1);
-        setActive((prev) => !prev);
-        setTimeout(() => {
-          containerRef.current.style.left = index * 101 + "%";
-          containerRef.current.style.backgroundColor = slides[index]?.color;
-        }, 500);
-        setTimeout(() => {
-          setActive((prev) => !prev);
-        }, 1000);
-      }
+      setIndex((prev) => (prev > 0 ? prev - 1 : data.length - 1));
     } else {
-      if (index < slides.length - 1) {
-        setIndex((prev) => prev + 1);
-        setActive((prev) => !prev);
-        setTimeout(() => {
-          containerRef.current.style.left = `-${(index + 1) * 101}%`;
-          containerRef.current.style.backgroundColor = slides[index + 1]?.color;
-        }, 500);
-        setTimeout(() => {
-          setActive((prev) => !prev);
-        }, 1000);
-      } else {
-        setIndex(0);
-        setActive((prev) => !prev);
-        setTimeout(() => {
-          containerRef.current.style.left = "0%";
-          containerRef.current.style.backgroundColor = slides[0]?.color;
-        }, 500);
-        setTimeout(() => {
-          setActive((prev) => !prev);
-        }, 1000);
-      }
+      setIndex((prev) => (prev < data.length - 1 ? prev + 1 : 0));
     }
+
+    setTimeout(() => {
+      containerRef.current.style.left = `-${index * 101}%`;
+      containerRef.current.style.backgroundColor = data[index]?.color;
+    }, 500);
+
+    setTimeout(() => {
+      setActive((prev) => !prev);
+    }, 1000);
   };
+
   return (
     <div className="slider">
       <div className="btn btn-prev" onClick={() => swipe("prev")}>
@@ -120,11 +61,10 @@ const Slider = ({ language }) => {
             strokeWidth="0.48426600000000003"
           />
           <g id="SVGRepo_iconCarrier">
-            {" "}
             <path
               id="XMLID_6_"
-              d="M237.739,4.394c-5.857-5.857-15.355-5.858-21.213,0L30,190.92V174.1c0-8.284-6.716-15-15-15s-15,6.716-15,15 v53.033c0,8.284,6.716,15,15,15h53.033c8.284,0,15-6.716,15-15c0-8.284-6.716-15-15-15h-16.82L237.739,25.607 C243.598,19.75,243.598,10.252,237.739,4.394z"
-            />{" "}
+              d="M237.739,4.394c-5.857-5.857-15.355-5.858-21.213,0L30,190.92V174.1c0-8.284-6.716-15-15-15s-15,6.716-15,15v53.033c0,8.284,6.716,15,15,15h53.033c8.284,0,15-6.716,15-15c0-8.284-6.716-15-15-15h-16.82L237.739,25.607C243.598,19.75,243.598,10.252,237.739,4.394z"
+            />
           </g>
         </svg>
       </div>
@@ -150,11 +90,10 @@ const Slider = ({ language }) => {
             strokeWidth="0.48426600000000003"
           />
           <g id="SVGRepo_iconCarrier">
-            {" "}
             <path
               id="XMLID_6_"
-              d="M237.739,4.394c-5.857-5.857-15.355-5.858-21.213,0L30,190.92V174.1c0-8.284-6.716-15-15-15s-15,6.716-15,15 v53.033c0,8.284,6.716,15,15,15h53.033c8.284,0,15-6.716,15-15c0-8.284-6.716-15-15-15h-16.82L237.739,25.607 C243.598,19.75,243.598,10.252,237.739,4.394z"
-            />{" "}
+              d="M237.739,4.394c-5.857-5.857-15.355-5.858-21.213,0L30,190.92V174.1c0-8.284-6.716-15-15-15s-15,6.716-15,15v53.033c0,8.284,6.716,15,15,15h53.033c8.284,0,15-6.716,15-15c0-8.284-6.716-15-15-15h-16.82L237.739,25.607C243.598,19.75,243.598,10.252,237.739,4.394z"
+            />
           </g>
         </svg>
       </div>
@@ -162,24 +101,22 @@ const Slider = ({ language }) => {
         className="container"
         ref={containerRef}
         style={{
-          backgroundColor: slides[0]?.color,
-          width: slides.length * 100.5 + "vw",
+          backgroundColor: data[0]?.color,
+          width: `${data.length * 100.5}vw`,
         }}
       >
-        {slides.map((slide) => {
-          return (
-            <div className="slide" key={slide.i}>
-              <h1 className={active ? "active" : ""}>
-                {slide.title?.[language]}
-              </h1>
-              <img
-                className={active ? "active" : ""}
-                src={`/assets/images/${slide.img}`}
-                alt={slide.title}
-              />
-            </div>
-          );
-        })}
+        {data.map((slide, i) => (
+          <div className="slide" key={i}>
+            <h1 className={active ? "active" : ""}>
+              {slide.title?.[language]}
+            </h1>
+            <img
+              className={active ? "active" : ""}
+              src={`${slide.img}`}
+              alt={slide.title?.[language]}
+            />
+          </div>
+        ))}
       </div>
     </div>
   );
