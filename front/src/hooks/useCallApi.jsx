950616851649061ad1ca1197569dc79731bd9a01
koussayfridhii <koussayfridhi@issatkas.u-kairouan.tn>
2024-06-10 16:59:44 +0100

    minor fix

--- front/src/hooks/useCallApi.jsx
+++ front/src/hooks/useCallApi.jsx
@@ -2,6 +2,7 @@ import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 
 const fetchApiData = async (linkSuffix) => {
+  const res = await axios.get(`https://crrhab-3ofe.vercel.app/`);
   const { data } = await axios.get(
     `https://crrhab-3ofe.vercel.app/api/${linkSuffix}`
   );
