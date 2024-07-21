8f46b552a9016e7681819c4aad7f3d6354ef4640
koussayfridhii <koussayfridhi@hotmail.fr>
2024-07-21 12:23:21 +0100

    minor fix

--- back/routes/search.route.js
+++ back/routes/search.route.js
@@ -6,6 +6,6 @@ const router = express.Router();
 const { searchAllCollections } = require('../controllers/search.controller');
 
 // Define route for searching all collections
-router.get('/', searchAllCollections);
+router.get('/search', searchAllCollections);
 
 module.exports = router;
