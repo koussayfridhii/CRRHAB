aa60e472841689ec032af13a8a8506f029702de3
koussayfridhii <koussayfridhi@hotmail.fr>
2024-07-25 20:13:06 +0100

    same thing

--- front/sitemap-generator.js
+++ front/sitemap-generator.js
@@ -1,17 +1,21 @@
-const path = require('path');
-const fs = require('fs');
-const Sitemap = require('react-router-sitemap').default;
-const router = require('./src/routes').default; // Adjust the path to your routes file
-
-function generateSitemap() {
-  const hostname = 'http://crrhab.agrinet.tn/';
-  const dest = path.resolve('./public', 'sitemap.xml');
-
-  return (
-    new Sitemap(router)
-      .build(hostname)
-      .save(dest)
-  );
-}
-
-generateSitemap();
+require('@babel/register')({
+    presets: ['@babel/preset-env', '@babel/preset-react']
+  });
+  
+  const path = require('path');
+  const Sitemap = require('react-router-sitemap').default;
+  const router = require('./src/routes').default; // Adjust the path to your routes file
+  
+  function generateSitemap() {
+    const hostname = 'http://crrhab.agrinet.tn/';
+    const dest = path.resolve('./public', 'sitemap.xml');
+  
+    return (
+      new Sitemap(router)
+        .build(hostname)
+        .save(dest)
+    );
+  }
+  
+  generateSitemap();
+  
