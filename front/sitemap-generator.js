1c64645e47475a6f23e4d566292da2d0a97e7bb5
koussayfridhii <koussayfridhi@hotmail.fr>
2024-07-25 20:18:17 +0100

    shitty fix

--- front/sitemap-generator.js
+++ front/sitemap-generator.js
@@ -1,21 +1,15 @@
-require('@babel/register')({
-    presets: ['@babel/preset-env', '@babel/preset-react']
-  });
-  
-  const path = require('path');
-  const Sitemap = require('react-router-sitemap').default;
-  const router = require('./src/routes').default; // Adjust the path to your routes file
-  
-  function generateSitemap() {
-    const hostname = 'http://crrhab.agrinet.tn/';
-    const dest = path.resolve('./public', 'sitemap.xml');
-  
-    return (
-      new Sitemap(router)
-        .build(hostname)
-        .save(dest)
-    );
-  }
-  
-  generateSitemap();
-  
+import 'babel-register';
+import path from 'path';
+import { default as Sitemap } from 'react-router-sitemap';
+import { default as router } from './src/routes'; // Adjust the path to your routes file
+
+function generateSitemap() {
+  const hostname = 'http://crrhab.agrinet.tn/';
+  const dest = path.resolve('./public', 'sitemap.xml');
+
+  return new Sitemap(router)
+    .build(hostname)
+    .save(dest);
+}
+
+generateSitemap();
