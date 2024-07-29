aa31c5b24bdab0b33ffbf3f8e3e857e990e31cb5
koussayfridhii <koussayfridhi@hotmail.fr>
2024-07-29 23:00:00 +0100

    test

--- back/app.js
+++ back/app.js
@@ -6,48 +6,54 @@ const passport = require("passport");
 const cors = require("cors");
 const { app, server } = require("./socket/socket");
 
-//routes
-const pagesRouter = require("./routes/pages.route");
-const userRouter = require("./routes/user.route");
-const formRouter = require("./routes/form.route");
-const mediaRouter = require("./routes/media.route");
-const newsRouter = require("./routes/news.route");
-const eventsRouter = require("./routes/event.route");
-const searchStationsRouter = require("./routes/searchStation.route");
-const searchStructuresRouter = require("./routes/searchStructure.route");
-const partnersRouter = require("./routes/partners.route");
-const documentsRouter = require("./routes/document.route");
-const linksRouter = require("./routes/link.route");
-const faqsRouter = require("./routes/faq.route");
-const contactUsRouter = require("./routes/contactUs.route");
-const surveysRouter = require("./routes/survey.route");
-const messagesRouter = require("./routes/message.route");
-const searchAllRouter = require("./routes/search.route");
-const ScientificCouncilMembersRouter = require("./routes/scientificCounciMembers.route");
-const statsRouter = require("./routes/stats.route");
-const researchTeamRouter = require("./routes/researchTeam.route");
-const diplomaCourseRouter = require("./routes/diplomaCourse.route");
-const scientificProductionRouter = require("./routes/scientificProduction.route");
-const laboratoryMembersRouter = require("./routes/laboratoryMembers.route");
-const nationalProjectRouter = require("./routes/nationalProject.route");
-const videoRouter = require("./routes/video.route");
-const paragraphLandingPageRoute = require("./routes/paragraphLanding.route");
-const historyRoute = require("./routes/history.route");
-const missionRoute = require("./routes/mission.route");
-const openDataRoute = require("./routes/openData.route");
-const searchRoute = require("./routes/search.route");
+// Routes
+const routes = {
+  pages: require("./routes/pages.route"),
+  user: require("./routes/user.route"),
+  form: require("./routes/form.route"),
+  media: require("./routes/media.route"),
+  news: require("./routes/news.route"),
+  events: require("./routes/event.route"),
+  searchStations: require("./routes/searchStation.route"),
+  searchStructures: require("./routes/searchStructure.route"),
+  partners: require("./routes/partners.route"),
+  documents: require("./routes/document.route"),
+  links: require("./routes/link.route"),
+  faqs: require("./routes/faq.route"),
+  contactUs: require("./routes/contactUs.route"),
+  surveys: require("./routes/survey.route"),
+  messages: require("./routes/message.route"),
+  searchAll: require("./routes/search.route"),
+  scientificCouncilMembers: require("./routes/scientificCounciMembers.route"),
+  stats: require("./routes/stats.route"),
+  researchTeam: require("./routes/researchTeam.route"),
+  diplomaCourse: require("./routes/diplomaCourse.route"),
+  scientificProduction: require("./routes/scientificProduction.route"),
+  laboratoryMembers: require("./routes/laboratoryMembers.route"),
+  nationalProject: require("./routes/nationalProject.route"),
+  video: require("./routes/video.route"),
+  paragraphLandingPage: require("./routes/paragraphLanding.route"),
+  history: require("./routes/history.route"),
+  mission: require("./routes/mission.route"),
+  openData: require("./routes/openData.route"),
+  search: require("./routes/search.route")
+};
 
-//middlewares
+// Middlewares
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(passport.initialize());
-const allowedOrigins = ["http://crrhab.agrinet.tn/","https://crrhab.agrinet.tn/","https://crrhab.vercel.app/" ,"http://193.95.21.154", "http://193.95.21.154"];
-app.options('*', cors()); // Handle preflight requests for all routes
+
+const allowedOrigins = [
+  "https://crrhab.agrinet.tn",
+  "https://crrhab.vercel.app",
+  "http://193.95.21.154"
+];
 
 app.use(cors({
-  origin: function (origin, callback) {
+  origin: (origin, callback) => {
     if (!origin || allowedOrigins.includes(origin)) {
       callback(null, true);
     } else {
@@ -59,6 +65,7 @@ app.use(cors({
   preflightContinue: false,
   optionsSuccessStatus: 204
 }));
+
 app.use((req, res, next) => {
   console.log('Request Headers:', req.headers);
   next();
@@ -67,46 +74,41 @@ app.use((req, res, next) => {
 require("./middlewares/passport")(passport);
 
 // Database connection
-mongoose
-  .connect(process.env.MONGO_URI , { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => {
-    console.log("DB Connected");
-  })
-  .catch((err) => {
-    console.log(err);
-  });
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("DB Connected"))
+  .catch(err => console.log(err));
 
 // Routes usage
 app.use("/api", [
-  pagesRouter,
-  userRouter,
-  formRouter,
-  mediaRouter,
-  newsRouter,
-  eventsRouter,
-  searchStationsRouter,
-  searchStructuresRouter,
-  partnersRouter,
-  documentsRouter,
-  linksRouter,
-  contactUsRouter,
-  faqsRouter,
-  surveysRouter,
-  messagesRouter,
-  searchAllRouter,
-  ScientificCouncilMembersRouter,
-  statsRouter,
-  researchTeamRouter,
-  diplomaCourseRouter,
-  scientificProductionRouter,
-  laboratoryMembersRouter,
-  nationalProjectRouter,
-  videoRouter,
-  paragraphLandingPageRoute,
-  historyRoute,
-  missionRoute,
-  openDataRoute,
-  searchRoute
+  routes.pages,
+  routes.user,
+  routes.form,
+  routes.media,
+  routes.news,
+  routes.events,
+  routes.searchStations,
+  routes.searchStructures,
+  routes.partners,
+  routes.documents,
+  routes.links,
+  routes.contactUs,
+  routes.faqs,
+  routes.surveys,
+  routes.messages,
+  routes.searchAll,
+  routes.scientificCouncilMembers,
+  routes.stats,
+  routes.researchTeam,
+  routes.diplomaCourse,
+  routes.scientificProduction,
+  routes.laboratoryMembers,
+  routes.nationalProject,
+  routes.video,
+  routes.paragraphLandingPage,
+  routes.history,
+  routes.mission,
+  routes.openData,
+  routes.search
 ]);
 
 app.get("/", (req, res) => {
