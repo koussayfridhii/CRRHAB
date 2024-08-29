1b46e8b1d079eed06816a516ac98d1789e8afedc
koussayfridhii <koussayfridhi@hotmail.fr>
2024-08-29 23:05:47 +0100

    fix

--- front/src/pages/Actualities.jsx
+++ front/src/pages/Actualities.jsx
@@ -106,6 +106,7 @@ const Actualities = () => {
                 >
                   {doc.title?.[language]}
                 </chakra.a>
+                , {doc?.createdAt}
             </ListItem>
           ))}
         </List>
